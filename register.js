cd61540279952b8187e8159a82f51e7cd6481310
AJIRI OSAUZO JEFFREY <ajirios@peppermultimedia.com>
2017-10-23 14:29:15 -0400

    Add files via upload

---
+++ register.js
@@ -0,0 +1,33 @@
+﻿
+
+var roller = document.getElementById("epic");
+            var picArray = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"];
+            var index = 0;
+            
+            function slidePic()
+            
+            {
+                roller.setAttribute("src", picArray[index]);
+                index++;
+                
+                if (index == picArray.length)
+                    
+                    {
+                        index = 0;
+                    }
+                    
+            }
+            
+            setInterval(slidePic, 3000);
+
+function myFunction() {
+    document.getElementById("toggle").style.display = "block";
+    document.getElementById("toggle-down").style.display = "none";
+    document.getElementById("toggle-up").style.display = "block";
+}
+
+function myFunction2() {
+    document.getElementById("toggle").style.display = "none";
+    document.getElementById("toggle-down").style.display = "block";
+    document.getElementById("toggle-up").style.display = "none";
+}
